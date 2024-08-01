de4b01d4ff1c05e8d927de90ea332576dc1127a0
Magnus Andersson <mpja69@gmail.com>
2024-08-01 22:13:21 +0200

    Added cfg

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
+import "github.com/mpja69/pokedexcli/internal/pokeapi"
+
+type config struct {
+	Client   pokeapi.Client
+	Next     *string
+	Previous *string
+}
+
 func main() {
-	startRepl()
+	cfg := config{
+		Client: pokeapi.NewClient(),
+	}
+	// config := config{"https://pokeapi.co/api/v2/location-area/", "https://pokeapi.co/api/v2/location-area/"}
+	startRepl(&cfg)
 }
